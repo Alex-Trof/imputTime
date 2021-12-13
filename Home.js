ec1c484e059d1c8d31ca6fd4baeb1b0568cd26e3
Alex-Trof <trofimov1998@gmail.com>
2021-12-13 23:26:48 +0100

    Ajout champs

--- Home.js
+++ Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native';
 
 class Home extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Yo</Text>
+                <Text>Matin</Text>
+                <View style={styles.row}>
+                    <TextInput style={styles.textInput}/>
+                    <TextInput style={styles.textInput}/>
+                </View>
+
+                <Text>Après-midi</Text>
+                <View style={styles.row}>
+                    <TextInput style={styles.textInput}/>
+                    <TextInput style={styles.textInput}/>
+                </View>
             </View>
         )
     }
@@ -14,8 +25,23 @@ class Home extends React.Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      borderWidth: 1,
       justifyContent: 'center',
       alignItems: 'center'
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        width: '30%',
+        borderRadius: 10
+    },
+    row: {
+        flex: 0.04,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: '7%',
+        marginTop: '3%'
     },
   });
 
